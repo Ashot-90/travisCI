7575daf9f80f12e0dc6a2a27ad4ac545bffcddc0
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-09 15:42:25 +0400

    Update bring_up_and_run_tests.sh

--- bring_up_and_run_tests.sh
+++ bring_up_and_run_tests.sh
@@ -1,7 +1,7 @@
 cd /tmp/ta_assignment
 npm install
 npm audit fix
-nohup bash -c "npm run -s start-api &"
-nohup bash -c "npm start &"
+npm run -s start-api &
+npm start &
 sleep 10
 ./node_modules/cypress/bin/cypress run
